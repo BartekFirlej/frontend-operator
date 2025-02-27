c6f32125b352b97edba1d14a7f9db5aa4ad26438
yari12 <yari12@wp.pl>
2025-02-27 16:02:05 +0100

    Added window with freezed frame

--- src/components/RightPanel.jsx
+++ src/components/RightPanel.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 
-const RightPanel = ({ zoom, handleZoomIn, handleZoomOut, handleShowReportPopup }) => {
+const RightPanel = ({ zoom, handleZoomIn, handleZoomOut, handleShowReportPopup, handleCapture  }) => {
     return (
         <div id="video-controls" class="absolute top-2.5 right-2.5 flex flex-col gap-2.5">
             <button id="zoom" class="w-[120px] opacity-50 bg-[rgba(0,0,0,0.5)] text-white border-0 p-2.5 cursor-pointer transition-opacity duration-300 z-[2] hover:opacity-100">Zoom: {zoom.toFixed(1)}</button>
             <button onClick={handleZoomIn} class="w-[120px] opacity-50 bg-[rgba(0,0,0,0.5)] text-white border-0 p-2.5 cursor-pointer transition-opacity duration-300 z-[2] hover:opacity-100">Przybliż</button>
             <button onClick={handleZoomOut} class="w-[120px] opacity-50 bg-[rgba(0,0,0,0.5)] text-white border-0 p-2.5 cursor-pointer transition-opacity duration-300 z-[2] hover:opacity-100">Oddal</button>
-            <button id="play-pause" class="w-[120px] opacity-50 bg-[rgba(0,0,0,0.5)] text-white border-0 p-2.5 cursor-pointer transition-opacity duration-300 z-[2] hover:opacity-100">Stopklatka</button>
+            <button onClick={handleCapture} class="w-[120px] opacity-50 bg-[rgba(0,0,0,0.5)] text-white border-0 p-2.5 cursor-pointer transition-opacity duration-300 z-[2] hover:opacity-100">Stopklatka</button>
             <button id="report" class="w-[120px] opacity-50 bg-[rgba(0,0,0,0.5)] text-white border-0 p-2.5 cursor-pointer transition-opacity duration-300 z-[2] hover:opacity-100"
                 onClick={handleShowReportPopup}>
                 Zgłoś</button>
