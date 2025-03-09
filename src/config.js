54ffa5e1d166ece691282b9a98e30f6c9383aa1f
yari12 <yari12@wp.pl>
2025-03-09 22:45:42 +0100

    config update

--- src/config.js
+++ src/config.js
@@ -1,7 +1,10 @@
 const config = {
+    GATEWAY_LOCALHOST: "http://localhost:5047",
     WS_ENDPOINT: "ws://localhost:8765",
     VIDEO_FEED_URL: "http://localhost:5000/video_feed",
-    FLIGHT_BEGIN_ENDPOINT: "http://localhost:5000/flight"
+    FLIGHT_BEGIN_ENDPOINT: `${GATEWAY_LOCALHOST}/flights`,
+    SIGNAL_ENDPOINT: `${GATEWAY_LOCALHOST}/signals`,
+    TARGET_ENDPOINT: `${GATEWAY_LOCALHOST}/targets`
   };
   
   export default config;
