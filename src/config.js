b424853ae69efb97676cb71c7e3ec3717f5ef3a0
BartekFirlej <yari12@wp.pl>
2025-03-11 17:54:18 +0100

    Updated uris in config

--- src/config.js
+++ src/config.js
@@ -1,10 +1,12 @@
+const GATEWAY_LOCALHOST = "http://localhost:5147";
+
 const config = {
-    GATEWAY_LOCALHOST: "http://localhost:5047",
-    WS_ENDPOINT: "ws://localhost:8765",
-    VIDEO_FEED_URL: "http://localhost:5000/video_feed",
-    FLIGHT_BEGIN_ENDPOINT: `${GATEWAY_LOCALHOST}/flights`,
-    SIGNAL_ENDPOINT: `${GATEWAY_LOCALHOST}/signals`,
-    TARGET_ENDPOINT: `${GATEWAY_LOCALHOST}/targets`
-  };
-  
-  export default config;
+  GATEWAY_LOCALHOST,
+  WS_ENDPOINT: "ws://localhost:8765",
+  VIDEO_FEED_URL: "http://localhost:5000/video_feed",
+  FLIGHT_BEGIN_ENDPOINT: `${GATEWAY_LOCALHOST}/flights`,
+  SIGNAL_ENDPOINT: `${GATEWAY_LOCALHOST}/signals`,
+  TARGET_ENDPOINT: `${GATEWAY_LOCALHOST}/targets`
+};
+
+export default config;
